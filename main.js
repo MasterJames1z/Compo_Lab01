39224c665473ce4fb1decf6014a83a719ca57a77
Worawut Aunkham <jameskater350@gmail.com>
2022-07-01 01:07:06 +0700

    Add condition inStock

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const app = Vue.createApp({
     data(){
         return {
             // product: 'Socks'
-            product: 'Boots',
+            // product: 'Boots',
             description: 'This boots is difference',
-            image: './assets/images/socks_green.jpg',
+            // image: './assets/images/socks_green.jpg',
             link: 'http://www.camt.cmu.ac.th',
+
+            product: 'Shoes',
+            image: './assets/images/socks_green.jpg',
+            inStock: true
         }
     }
 })
